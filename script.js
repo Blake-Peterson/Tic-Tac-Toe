a3383e58d6174c6a6db57046c8411776b7717f00
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2024-09-11 09:36:30 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function Gameboard(){
+    const board =[];
+
+    const createboard = () =>{
+        for(let i=0;i<3;i++){
+            board[i] = [];
+            for(let j=0;j<3;j++){
+                board[i].push(0);
+            }
+        }
+    }
+    console.log(board);
+}
+
+function displayGame(){
+    Gameboard();
+
+}
