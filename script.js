724fd9518227e71e0ca4b8444c891453e3c24860
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2024-09-15 15:21:11 -0500

    added functions to GameController for player turns and getting active player

--- script.js
+++ script.js
@@ -34,7 +34,8 @@ function Cell(){
     };
 
     const getValue = () => value;
-    return { addToken,getValue};
+
+    return { addMarker,getValue};
 }
 
 function Player(name,marker){
@@ -42,11 +43,69 @@ function Player(name,marker){
 }
 
 function GameController(){
+    const board = Gameboard();
+    
+    const players = [
+        player1 = new Player("Mark","X"),
+        player2 = new Player("Bob the Builder","O")
+    ];
 
+    let activePlayer = player[0];
+    
+    const switchPlayerTurn = ()=> {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    }
+
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = ()=>{
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn.`);
+    };
+
+    const playTurn = (row,column) => {
+        console.log(`Placing ${getActivePlayer().name}'s symbol into row ${row} column ${column}...`);
+        board.placeMarker(row,column,getActivePlayer().marker);
+
+        /* Check board for a winning condition*/
+        const winningConditions = [
+            //Horizontal
+            [[0,0], [0,1], [0,2]],
+            [[1,0], [1,1], [1,2]],
+            [[2,0], [2,1], [2,2]],
+            //Vertical
+            [[0,0], [1,0], [2,0]],
+            [[0,1], [1,1], [2,1]],
+            [[0,2], [1,2], [2,2]],
+            //Diagonal
+            [[0,0], [1,1], [2,2]],
+            [[0,2], [1,1], [2,0]],
+        ];
+
+        const checkForWinner = (board)=>{
+            for(let condition of winningConditions){
+                const [a,b,c] = condition;
+                if(board[a[0]][a[1]] !== null && 
+                    board[a[0]][a[1]] === board[b[0]][b[1]] &&
+                     board[a[0]][a[1]] === board[c[0]][c[1]]){
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        checkForWinner();
+        switchPlayerTurn();
+        printNewRound();
+    };
+
+
+    return { getActivePlayer, playTurn};
 }
 
 function DisplayGame(){
-    Gameboard();
+    let game = Gameboard();
     
 
 }
